3c8c02aa552121658c5ea8a34a4d144910674874
Pratik Paduri <87135097+hilobot4106@users.noreply.github.com>
2024-02-21 15:48:53 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,46 @@
-
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . 3 3 . . . . . . . . . . . . 
+        . . . . 3 3 3 3 3 3 . . . 3 . . 
+        . . . . . . . . . . 3 3 3 3 . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite, 50, 50)
+})
+let projectile: Sprite = null
+let mySprite: Sprite = null
+let count = 0
+scene.setBackgroundColor(6)
+let list = [tilemap`level7`, tilemap`level1`, tilemap`level4`]
+mySprite = sprites.create(img`
+    . . . . . f f f f f . . . . . . 
+    . . . . f e e e e e f . . . . . 
+    . . . f d d d d d e e f . . . . 
+    . . f f f d d f f d e f f . . . 
+    . c d d e e d d d d e d d f . . 
+    . c c d d d d c d d e d f f f . 
+    . c d c c c c d d d e d f b d f 
+    . . c d d d d d d e e f f d d f 
+    . . . c d d d d e e f f e f f f 
+    . . . . f f f e e f e e e f . . 
+    . . . . f e e e e e e e f f f . 
+    . . . f e e e e e e f f f e f . 
+    . . f f e e e e f f f f f e f . 
+    . f b d f e e f b b f f f e f . 
+    . f d d f e e f d d b f f f f . 
+    . f f f f f f f f f f f f f . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+tiles.setCurrentTilemap(list[0])
+scene.cameraFollowSprite(mySprite)
