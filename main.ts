d7776163a83b892a04d872f7bb23d0984095f9af
Pratik Paduri <87135097+hilobot4106@users.noreply.github.com>
2024-02-27 15:34:05 -0600

    finished

--- main.ts
+++ main.ts
@@ -5,7 +5,27 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     movedown = false
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (mySprite.vx == 0 && mySprite.vy == 0) {
+    if (moveleft == true) {
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . 9 9 9 9 9 9 9 9 9 9 9 9 . . 
+            . . 9 9 9 9 9 9 9 9 9 9 9 9 . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite, -200, 0)
+    }
+    if (moveright == true) {
         projectile = sprites.createProjectileFromSprite(img`
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
@@ -24,87 +44,46 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             `, mySprite, 200, 0)
-    } else {
-        if (moveleft == true) {
-            projectile = sprites.createProjectileFromSprite(img`
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . 9 9 9 9 9 9 9 9 9 9 9 9 . . 
-                . . 9 9 9 9 9 9 9 9 9 9 9 9 . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                `, mySprite, -200, 0)
-        }
-        if (moveright == true) {
-            projectile = sprites.createProjectileFromSprite(img`
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . 9 9 9 9 9 9 9 9 9 9 9 9 . . 
-                . . 9 9 9 9 9 9 9 9 9 9 9 9 . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                `, mySprite, 200, 0)
-        }
-        if (movedown == true) {
-            projectile = sprites.createProjectileFromSprite(img`
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . 9 9 . . . . . . . 
-                . . . . . . . 9 9 . . . . . . . 
-                . . . . . . . 9 9 . . . . . . . 
-                . . . . . . . 9 9 . . . . . . . 
-                . . . . . . . 9 9 . . . . . . . 
-                . . . . . . . 9 9 . . . . . . . 
-                . . . . . . . 9 9 . . . . . . . 
-                . . . . . . . 9 9 . . . . . . . 
-                . . . . . . . 9 9 . . . . . . . 
-                . . . . . . . 9 9 . . . . . . . 
-                . . . . . . . 9 9 . . . . . . . 
-                . . . . . . . 9 9 . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                `, mySprite, 0, 200)
-        }
-        if (moveup == true) {
-            projectile = sprites.createProjectileFromSprite(img`
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . 9 9 . . . . . . . 
-                . . . . . . . 9 9 . . . . . . . 
-                . . . . . . . 9 9 . . . . . . . 
-                . . . . . . . 9 9 . . . . . . . 
-                . . . . . . . 9 9 . . . . . . . 
-                . . . . . . . 9 9 . . . . . . . 
-                . . . . . . . 9 9 . . . . . . . 
-                . . . . . . . 9 9 . . . . . . . 
-                . . . . . . . 9 9 . . . . . . . 
-                . . . . . . . 9 9 . . . . . . . 
-                . . . . . . . 9 9 . . . . . . . 
-                . . . . . . . 9 9 . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . . . . . . . 
-                `, mySprite, 0, -200)
-        }
+    }
+    if (movedown == true) {
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 9 9 . . . . . . . 
+            . . . . . . . 9 9 . . . . . . . 
+            . . . . . . . 9 9 . . . . . . . 
+            . . . . . . . 9 9 . . . . . . . 
+            . . . . . . . 9 9 . . . . . . . 
+            . . . . . . . 9 9 . . . . . . . 
+            . . . . . . . 9 9 . . . . . . . 
+            . . . . . . . 9 9 . . . . . . . 
+            . . . . . . . 9 9 . . . . . . . 
+            . . . . . . . 9 9 . . . . . . . 
+            . . . . . . . 9 9 . . . . . . . 
+            . . . . . . . 9 9 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite, 0, 200)
+    }
+    if (moveup == true) {
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 9 9 . . . . . . . 
+            . . . . . . . 9 9 . . . . . . . 
+            . . . . . . . 9 9 . . . . . . . 
+            . . . . . . . 9 9 . . . . . . . 
+            . . . . . . . 9 9 . . . . . . . 
+            . . . . . . . 9 9 . . . . . . . 
+            . . . . . . . 9 9 . . . . . . . 
+            . . . . . . . 9 9 . . . . . . . 
+            . . . . . . . 9 9 . . . . . . . 
+            . . . . . . . 9 9 . . . . . . . 
+            . . . . . . . 9 9 . . . . . . . 
+            . . . . . . . 9 9 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite, 0, -200)
     }
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -119,18 +98,57 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     moveup = false
     movedown = false
 })
+function enemyspawn (number: number, speed: number) {
+    for (let index = 0; index < number; index++) {
+        enemy = sprites.create(img`
+            . . . . . . . e c 7 . . . . . . 
+            . . . . e e e c 7 7 e e . . . . 
+            . . c e e e e c 7 e 2 2 e e . . 
+            . c e e e e e c 6 e e 2 2 2 e . 
+            . c e e e 2 e c c 2 4 5 4 2 e . 
+            c e e e 2 2 2 2 2 2 4 5 5 2 2 e 
+            c e e 2 2 2 2 2 2 2 2 4 4 2 2 e 
+            c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+            c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+            c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+            c e e 2 2 2 2 2 2 2 2 2 2 4 2 e 
+            . e e e 2 2 2 2 2 2 2 2 2 4 e . 
+            . 2 e e 2 2 2 2 2 2 2 2 4 2 e . 
+            . . 2 e e 2 2 2 2 2 4 4 2 e . . 
+            . . . 2 2 e e 4 4 4 2 e e . . . 
+            . . . . . 2 2 e e e e . . . . . 
+            `, SpriteKind.Enemy)
+        tiles.placeOnRandomTile(enemy, sprites.castle.tileDarkGrass2)
+        enemy.follow(mySprite, speed)
+    }
+}
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    sprites.destroy(sprite)
+    info.changeScoreBy(-1)
+    currentnumofenemy += -1
+})
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     moveleft = false
     moveright = false
     moveup = false
     movedown = true
 })
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    sprites.destroy(sprite)
+    info.changeScoreBy(-1)
+    currentnumofenemy += -1
+})
+let enemy: Sprite = null
 let projectile: Sprite = null
 let movedown = false
 let moveup = false
 let moveright = false
 let moveleft = false
 let mySprite: Sprite = null
+let tilemap2 = 0
+let levelnum = 0
 let count = 0
 scene.setBackgroundColor(6)
 let list = [tilemap`level7`, tilemap`level1`, tilemap`level4`]
@@ -152,10 +170,37 @@ mySprite = sprites.create(img`
     . f d d f e e f d d b f f f f . 
     . f f f f f f f f f f f f f . . 
     `, SpriteKind.Player)
-tiles.setCurrentTilemap(list[0])
 controller.moveSprite(mySprite)
 scene.cameraFollowSprite(mySprite)
 moveleft = false
 moveright = false
 moveup = false
 movedown = false
+info.setScore(5)
+let speedenemy = 15
+let numenemy = 5
+let currentnumofenemy = 0
+levelnum = 1
+info.setLife(3)
+forever(function () {
+    if (currentnumofenemy == 0) {
+        tiles.setCurrentTilemap(list[tilemap2])
+        info.setScore(numenemy)
+        game.splash("Level " + levelnum)
+        currentnumofenemy = numenemy
+        enemyspawn(numenemy, speedenemy)
+        numenemy += 3
+        speedenemy += 3
+        levelnum += 1
+        tilemap2 += 1
+        if (tilemap2 == 3) {
+            tilemap2 = 0
+        }
+    }
+    if (info.life() == 0) {
+        game.splash("Game Over")
+        game.splash("You made it to level " + levelnum)
+        sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
+        sprites.destroy(mySprite, effects.hearts, 500)
+    }
+})
